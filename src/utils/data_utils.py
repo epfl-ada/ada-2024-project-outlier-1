04e9307b1ec8af3aefd5e7d1514e0313f4a2b969
Julia Guignon <julia@julia-s.plane>
2024-11-15 18:14:28 +0100

    part1_result updated

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-__all__ = ['delete_duplicates_cat', 'art_not_in_cat', 'articles_in_common', 'paths_with_cat_only']
+__all__ = ['delete_duplicates_cat', 'art_not_in_cat', 'articles_in_common', 'paths_with_cat_only', 'compute_stats_games']
 
 def delete_duplicates_cat(df_cat, relationships):
     """
@@ -19,7 +19,7 @@ def delete_duplicates_cat(df_cat, relationships):
     """
     # loop on all ordering relationships defined
     for cat, subcat in relationships:
-        # create a df with the articles assigned in several main categories
+        # create/update a df with the articles assigned in several main categories
         categories_duplicates = df_cat.loc[df_cat.article.duplicated(keep=False)]
 
         # find the ones with the main category of interest
@@ -50,18 +50,24 @@ def articles_in_common(list1, list2) :
 
 
 def paths_with_cat_only(df_f, df_unf, df_cat, art_to_remove, verbose=True):
+    # df_cat = df_cat.set_index('article', drop=True)
+    
     init_rows_f = df_f.shape[0]
     init_rows_unf = df_unf.shape[0]
     
     df_f = df_f.drop(df_f[df_f.start.isin(art_to_remove)].index)
     df_f = df_f.drop(df_f[df_f.end.isin(art_to_remove)].index)
     df_f['source_cat'] = df_cat.main_category.loc[df_f.start].values
-    df_f['target_cat'] = df_cat.main_category.loc[df_f.end].values
+    df_f['target_cat'] = df_cat.main_category.loc[df_f.end].values    
+    # df_f['source_cat'] = df_cat.main_category.loc[df_f.start == df_cat.article].values
+    # df_f['target_cat'] = df_cat.main_category.loc[df_f.end == df_cat.article].values
     
     df_unf = df_unf.drop(df_unf[df_unf.start.isin(art_to_remove)].index)
     df_unf = df_unf.drop(df_unf[df_unf.target.isin(art_to_remove)].index)
     df_unf['source_cat'] = df_cat.main_category.loc[df_unf.start].values
     df_unf['target_cat'] = df_cat.main_category.loc[df_unf.target].values
+    # df_unf['source_cat'] = df_cat.main_category.loc[df_unf.start == df_cat.article].values
+    # df_unf['target_cat'] = df_cat.main_category.loc[df_unf.target == df_cat.article].values
 
     if verbose:
         # let's check how much data has been discarded
@@ -70,3 +76,20 @@ def paths_with_cat_only(df_f, df_unf, df_cat, art_to_remove, verbose=True):
     
     
     return df_f, df_unf
+
+
+def compute_stats_games(df, type_path):
+    df_stats = pd.DataFrame(df.index.value_counts().sort_index())
+    df_stats.index.names = ['start', 'target']
+    df_stats[f'avg_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).mean()
+    df_stats[f'std_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).std()
+    df_stats[f'sem_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).sem()
+    df_stats[f'med_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).median()
+    df_stats[f'q25_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).quantile(0.25)
+    df_stats[f'q75_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).quantile(0.75)
+    df_stats[f'q10_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).quantile(0.10)
+    df_stats[f'q90_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).quantile(0.90)
+    df_stats[f'min_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).min()
+    df_stats[f'max_{type_path}_path'] = df['path'].str.len().groupby(level=[0,1]).max()
+    
+    return df_stats

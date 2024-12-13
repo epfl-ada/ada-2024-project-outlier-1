5fa1d44da24b16825ad10602a18deeb41fa38f9a
Julia Guignon <julia@julia-s.plane>
2024-12-13 11:45:42 +0100

    solved conflicts in plotting

--- src/scripts/plotting.py
+++ src/scripts/plotting.py
@@ -7,6 +7,12 @@ import networkx as nx
 from matplotlib.patches import Patch
 import random
 
+import matplotlib as mpl
+from collections import defaultdict
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
+from math import log10
 
 
 __all__ = ['plot_average_links_per_page',
@@ -17,7 +23,13 @@ __all__ = ['plot_average_links_per_page',
            'plotting_difference_heatmap',
            'plot_degree_distribution',
            'get_sankey_data',
-           'plot_distribution_path_length']
+           'plot_distribution_path_length'
+           'get_sankey_data',
+           'get_multistep_sankey_data',
+           'plotly_save_to_html',
+           'plot_heatmap',
+           'colorscale_cmap',
+           'get_palette_cat']
 
 
 def plot_average_links_per_page(links2007, links2024, articles, graph_based=False) :
@@ -148,7 +160,8 @@ def plotting_difference_heatmap(spm1, spm2) :
 
 
 
-def get_sankey_data(df, categories, type_data, suffix_fn='1'):
+
+def get_sankey_data(df, categories, type_data, get_stats=False, suffix_fn='1'):
     """
     Produce formatted datafile to create a sankey diagram on https://app.flourish.studio/. 
 
@@ -177,22 +190,263 @@ def get_sankey_data(df, categories, type_data, suffix_fn='1'):
         print('type_data parameter unrecognized, return -1')
         return -1
     
-    grouped_by_cat = df.groupby([col1, col2])#.apply(lambda x: x).drop(columns=['source_cat', 'target_cat']).sort_index()
-    cat = list(categories.main_category.value_counts().sort_index().index)
+    cats = sorted(list(categories.main_category.unique()))
 
     # main_cat
     source = []
     target = []
     value = []
-    for (scat, ecat), group in grouped_by_cat:
-        source.append(scat)
-        target.append(ecat)
-        value.append(len(group))
-    sankey_data_finished = pd.DataFrame(zip(source, target, value), columns=['source', 'target', 'val'])
-    sankey_data_finished.to_csv(f'./data/sankey/sankey_data_{type_data}_v'+suffix_fn+'.csv', index=False)
-    distrib = np.array(value)/sum(value)
+    to_itself = defaultdict(float)
+
+    if get_stats:
+        means = defaultdict(float)
+        maxs = defaultdict(float)
+        mins = defaultdict(float)
+        links_per_cat = defaultdict(float)
+    
+    for scat in cats:
+        links_scat = 0
+        group = df.loc[df[col1]==scat]
+        tmp = []
+        for ecat in cats:
+            ggroup = group.loc[group[col2]==ecat]
+            source.append(scat)
+            target.append(ecat)
+            value.append(len(ggroup))
+            tmp.append(len(ggroup))
+            if scat == ecat:
+                to_itself[scat] = len(ggroup)
+        if get_stats:
+            tmp = np.array(tmp)
+            links_scat = sum(tmp)
+            to_itself[scat] = to_itself[scat]/links_scat
+            links_per_cat[scat] = links_scat
+            means[scat] = np.mean(tmp/links_scat)
+            maxs[scat] = np.max(tmp/links_scat)
+            mins[scat] = np.min(tmp/links_scat)
+
+    
+   
+
     tot_links = sum(value)
-    return distrib, tot_links
+    distrib = np.array(value)/tot_links
+    sankey_data_finished = pd.DataFrame(zip(source, target, value, distrib*100), columns=['source', 'target', 'val', 'percentage'])
+    sankey_data_finished.to_csv(f'./data/sankey/sankey_data_{type_data}_v'+suffix_fn+'.csv', index=False)
+
+    if get_stats:
+        return distrib, tot_links, {'mean': means, 'min': mins, 'max': maxs, 'to_itself': to_itself, 'links_per_scats':links_per_cat}
+    else:        
+        return distrib, tot_links
+
+
+
+def get_multistep_sankey_data(df, categories, get_stats=False, suffix_fn='1'):
+    """
+    Produce formatted datafile to create a sankey diagram with 2 steps on https://app.flourish.studio/. 
+
+    Args:
+        df (pandas.DataFrame): dataframe containing the data
+        categories (pandas.DataFrame): dataframe containing the categories of each article
+
+    Returns:
+        distrib (np.array of float): distribution of the number of links/paths between categories
+        tot_links (int): total number of links/paths
+    """
+    
+    cats = sorted(list(categories.main_category.unique()))
+
+    # main_cat
+    source = []
+    target = []
+    value_start2end = []
+    value_end2target = []
+    step_from = []
+    step_to = []
+
+    
+    # for scat, group in df.groupby(['source_cat']):
+    #     for ecat, egroup in group.groupby(['end_cat']):
+    #         source.append(scat[0])
+    #         target.append(ecat[0])
+    #         value_start2end.append(len(egroup))
+    #         step_from.append(0)
+    #         step_to.append(1)
+
+
+    # for ecat, group in df.groupby(['end_cat']):
+    #     for tcat, tgroup in group.groupby(['target_cat']):
+    #         source.append(ecat[0])
+    #         target.append(tcat[0])
+    #         value_end2target.append(len(tgroup))
+    #         step_from.append(1)
+    #         step_to.append(2)
+    
+    for scat in cats:
+        group = df.loc[df.source_cat == scat]
+        for ecat in cats:
+            egroup = group.loc[group.end_cat == ecat]
+            source.append(scat)
+            target.append(ecat)
+            value_start2end.append(len(egroup))
+            step_from.append(0)
+            step_to.append(1)
+
+
+
+    for ecat in cats:
+        group = df.loc[df.end_cat == ecat]
+        for tcat in cats:
+            tgroup = group.loc[group.target_cat == tcat]
+            source.append(ecat)
+            target.append(tcat)
+            value_end2target.append(len(tgroup))
+            step_from.append(1)
+            step_to.append(2)
+
+
+    tot_links_start2end = sum(value_start2end)
+    tot_links_end2target = sum(value_end2target)
+    distrib_start2end = np.array(value_start2end)/tot_links_start2end
+    distrib_end2target = np.array(value_end2target)/tot_links_end2target
+    distrib = np.concatenate((distrib_start2end, distrib_end2target))
+    sankey_data_finished = pd.DataFrame(zip(source, target, value_start2end+value_end2target, distrib*100, step_from, step_to), columns=['source', 'target', 'val', 'percentage', 'step_from', 'step_to'])
+    sankey_data_finished.to_csv(f'./data/sankey/sankey_data_multistep_v'+suffix_fn+'.csv', index=False)
+      
+    return distrib_start2end, distrib_end2target, tot_links_start2end, tot_links_end2target
+
+
+def plotly_save_to_html(fig, fn):
+    pio.show(fig)
+    pie_html = pio.to_html(fig)
+
+    with open(fn+'.html', 'w') as f:
+        f.write(pie_html)
+
+
+
+
+def plot_heatmap(vals, names, num_links, type_plot, vmin=0, vmax=0, gamma=0.47):
+    fig = go.Figure()
+
+    if type_plot=='links':
+        fn = 'links_categories'
+        title = 'Category flows'
+        xlabel = 'Link in'
+        ylabel = 'Link out'
+    elif type_plot=='unf_start': 
+        fn = f'categories_unfinished_paths_start2end'
+        title = f'Categories of start and end articles for unfinished paths'
+        xlabel = 'End article category'
+        ylabel = 'Source article category'
+    elif type_plot=='unf_target': 
+        fn = f'categories_unfinished_paths_end2target'
+        title = f'Categories of end and target articles for unfinished paths'
+        xlabel = 'Target article category'
+        ylabel = 'End article category'
+    else:
+        fn = f'categories_{type_plot}inished_paths_start2target'
+        title = f'Categories of start and target articles for {type_plot}inished paths'
+        xlabel = 'Target article category'
+        ylabel = 'Source article category'
+
+
+    all_to_cat_perc = np.array([np.sum(vals*100, axis=0)]*len(vals))
+    cat_to_all_perc = np.array([ [i]*len(vals) for i in np.sum(vals*100, axis=1)])
+
+    all_to_cat_counts = np.array([np.sum(vals*num_links, axis=0)]*len(vals))
+    cat_to_all_counts = np.array([ [i]*len(vals) for i in np.sum(vals*num_links, axis=1)])
+    
+    fig.add_trace(go.Heatmap(
+        z = vals*100,
+        x = names,
+        y = names,
+        customdata = np.dstack((np.round(vals*num_links,0), all_to_cat_perc, all_to_cat_counts, cat_to_all_perc, cat_to_all_counts)),
+        hovertemplate = "* → %{x}: %{customdata[1]:0.3f}%, %{customdata[2]} counts <br>" +
+            "%{y} → *: %{customdata[3]:0.3f}%, %{customdata[4]} counts <br>" +
+            "%{y} → %{x}: %{z:0.3f}%, %{customdata[0]} counts <extra></extra>",
+        hoverlabel_font_size = 18,
+        colorscale = colorscale_cmap('plasma', vals*100, gamma, vmin, vmax),
+    ))
+    
+    fig.update_layout(
+        width = 800,
+        height = 800,
+        font_size = 18,
+        yaxis_scaleanchor="x",
+        title = dict(
+            text = title,
+            xanchor = 'center',
+            x = 0.5)
+    )
+
+    fig.update_xaxes(
+        title_text = xlabel
+    )
+    
+    fig.update_yaxes(
+        title_text = ylabel
+    )
+
+    if vmax!=0:
+        fig.data[0].update(zmin=0, zmax=vmax)
+
+    plotly_save_to_html(fig, fn)
+
+
+def colorscale_cmap(cmap_name, data, gamma, vmin, vmax):
+    """
+    Generate a Plotly-compatible colorscale in an appropriate scale.
+
+    Parameters:
+        cmap_name (str): name of the Matplotlib colormap (e.g., 'plasma').
+        data (array-like): data array to compute the color scale range
+        gamma (float): change colormap scale
+        vmin (float): min. value for the colorscale
+        vmax (float): max. value for the colorscale
+    Returns:
+        colorscale (list): Colorscale for use in the colorscale attribute of go.Heatmap.
+    """
+    if np.min(data) < 0:
+        raise ValueError("Data must contain only positive values for an appropriate scale.")
+    
+    cmap = mpl.colormaps[cmap_name]
+    
+    if vmin==0 and vmax==0:
+        vmin, vmax = np.min(data), np.max(data)
+    norm = mpl.colors.PowerNorm(gamma, vmin=vmin, vmax=vmax)
+    
+    num_colors = 100
+    values = np.linspace(vmin, vmax, num_colors)
+    colors = [cmap(norm(v))[:3] for v in values]  # Extract RGB values
+    
+    # Create a Plotly-compatible colorscale
+    colorscale = [
+        [i / (num_colors - 1), f'rgb({int(r * 255)}, {int(g * 255)}, {int(b * 255)})']
+        for i, (r, g, b) in enumerate(colors)
+    ]
+    
+    return colorscale
+
+
+def get_palette_cat():
+    fancy_palette = {"Art": "#daa520",
+                    "Business_Studies": "#000000",
+                    "Countries": "#e31a1c",
+                    "Design_and_Technology": "#b2df8a",
+                    "Religion": "#fdbf6f",
+                    "Everyday_life": "#cab2d6",
+                    "Geography": "#fb9a99",
+                    "People": "#b15928",
+                    "Language_and_literature": "#33a02c",
+                    "IT": "#ff7f00",
+                    "History": "#eeee55",
+                    "Mathematics": "#6a3d9a",
+                    "Music": "#888888",
+                    "Citizenship": "#a6cee3",
+                    "Science": "#1f78b4"}
+    return fancy_palette
+
+
 
 def plot_degree_distribution(G_2007, G_2024):
     """
@@ -208,6 +462,7 @@ def plot_degree_distribution(G_2007, G_2024):
 
     ax.set(title='Distribution of the degrees of nodes', xlabel='degree', ylabel='fraction of nodes')
     plt.legend()
+    
 
 def plot_distribution_path_length(G_2007, G_2024, n_samples=100, start_hops=0, n_hops=10):
     """
